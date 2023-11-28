66f1234377c07edc6ecd3361c1339df579de19c3
hemedga <hemed@gurary.co.il>
2023-11-28 22:52:10 +0000

    Fixed Indetation

--- app/api/api_v0/endpoints/items.py
+++ app/api/api_v0/endpoints/items.py
@@ -3,41 +3,42 @@ from fastapi import APIRouter
 router = APIRouter(prefix='/items', tags=["Items"])
 d = [
 	{
-		"id": 0,
-		"inInventory": "false",
-		"product": "Car",
+        "id": 0,
+        "inInventory": "false",
+        "product": "Car",
     },
 	{
-		"id": 1,
-		"inInventory": "false",
-		"product": "Coat",
+        "id": 1,
+        "inInventory": "false",
+        "product": "Coat",
     },
 	{
-		"id": 2,
-		"inInventory": "true",
-		"product": "Cereal",
+        "id": 2,
+        "inInventory": "true",
+        "product": "Cereal",
     },
 	{
-		"id": 3,
-		"inInventory": "false",
-		"product": "Cerberos",
+        "id": 3,
+        "inInventory": "false",
+        "product": "Cerberos",
     },
 	{
-		"id": 4,
-		"inInventory": "false",
-		"product": "Cumin",
+        "id": 4,
+        "inInventory": "false",
+        "product": "Cumin",
     },
 	{
-		"id": 5,
-		"inInventory": "true",
-		"product": "Can",
-    }	
+        "id": 5,
+        "inInventory": "true",
+        "product": "Can",
+    }
+            
 ] 
 
 d5 = {
-		"id": 5,
-		"inInventory": "true",
-		"product": "Can",
+        "id": 5,
+        "inInventory": "true",
+        "product": "Can",
     }
 
 @router.get("")
