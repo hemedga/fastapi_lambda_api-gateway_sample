f0e555c53fa61ea180d06b3a19072ead542d44cf
hemedga <hemed@gurary.co.il>
2023-11-28 22:27:53 +0000

    populate items

--- app/api/api_v0/endpoints/items.py
+++ app/api/api_v0/endpoints/items.py
@@ -1,8 +1,49 @@
 from fastapi import APIRouter
 
 router = APIRouter(prefix='/items', tags=["Items"])
+d = [
+	{
+		"id": 0,
+		"inInventory": "false",
+		"product": "Car",
+    },
+	{
+		"id": 1,
+		"inInventory": "false",
+		"product": "Coat",
+    },
+	{
+		"id": 2,
+		"inInventory": "true",
+		"product": "Cereal",
+    },
+	{
+		"id": 3,
+		"inInventory": "false",
+		"product": "Cerberos",
+    },
+	{
+		"id": 4,
+		"inInventory": "false",
+		"product": "Cumin",
+    },
+	{
+		"id": 5,
+		"inInventory": "true",
+		"product": "Can",
+    }	
+] 
 
+d5 = {
+		"id": 5,
+		"inInventory": "true",
+		"product": "Can",
+    }
 
 @router.get("")
 async def root():
-    return {"message": "Get all items"}
+    return d
+
+@router.get("/5")
+def results():
+    return d5
